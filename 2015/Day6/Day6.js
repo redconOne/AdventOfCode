dbf32885014e57daa7294a247de2c3b141945097
redconOne <mingleeng@gmail.com>
2023-03-19 16:50:54 -0600

    add: solution part 2 for Day6

--- 2015/Day6/Day6.js
+++ 2015/Day6/Day6.js
@@ -5,22 +5,6 @@ const data = fs
   .replace(/\r/g, '')
   .trim();
 
-const changeLight = (task, x, y, lights) => {
-  switch (task) {
-    case 'on':
-      lights[y][x] = true;
-      break;
-    case 'off':
-      lights[y][x] = false;
-      break;
-    case 'toggle':
-      lights[y][x] = !lights[y][x];
-      break;
-    default:
-      console.log('Something went wrong in the switch.');
-  }
-};
-
 const solve1 = (str) => {
   const lights = [];
   let total = 0;
@@ -60,18 +44,66 @@ const test = `turn on 0,0 through 999,999
 turn off 0,0 through 999,999
 turn on 0,0 through 2,2`;
 
-const test2 = `turn off 0,0 through 4,4
-turn on 0,0 through 2,2
-turn off 0,0 through 0,0`;
-
-const test3 = `turn off 0,0 through 9,9
-turn on 0,0 through 9,9
-turn off 0,0 through 9,9
-turn on 0,0 through 1,1`;
-
 // console.log(solve1(test));
-// console.log(solve1(test2));
-// console.log(solve1(test3))
-console.log(solve1(data));
+// console.log(solve1(data));
 
-const solve2 = (str) => {};
+const solve2 = (str) => {
+  let total = 0;
+  let lights = [];
+
+  for (let y = 0; y < 1000; y++) {
+    let row = [];
+    for (let x = 0; x < 1000; x++) row.push(0);
+    lights.push(row);
+  }
+
+  for (const instruction of str.split('\n')) {
+    const regexp = instruction.startsWith('turn')
+      ? /turn (?<operation>.*) (?<startX>\d+),(?<startY>\d+) through (?<endX>\d+),(?<endY>\d+)/
+      : /(?<operation>.*) (?<startX>\d+),(?<startY>\d+) through (?<endX>\d+),(?<endY>\d+)/;
+
+    const { operation, startX, startY, endX, endY } =
+      instruction.match(regexp).groups;
+
+    for (let y = +startY; y <= +endY; y++) {
+      for (let x = +startX; x <= +endX; x++) {
+        if (operation === 'on') {
+          total += 1;
+          lights[y][x]++;
+        } else if (operation === 'off') {
+          total -= lights[y][x] > 0 ? 1 : 0;
+          if (lights[y][x] > 0) lights[y][x]--;
+        } else {
+          total += 2;
+          lights[y][x] += 2;
+        }
+      }
+    }
+  }
+
+  const temp = lights[0];
+
+  let result = 0;
+  let min = 0;
+  let max = 0;
+  for (const row of lights)
+    for (const light of row) {
+      min = Math.min(min, light);
+      max = Math.max(max, light);
+      result += light;
+      if (light < 0) console.log(light);
+    }
+
+  console.log(result);
+  console.log(min);
+  console.log(max);
+  return total;
+};
+
+const test2 = `turn on 0,0 through 0,0
+toggle 0,0 through 999,999`;
+
+// console.log(solve2(test2));
+console.log(solve2(data));
+// 13824059
+//
