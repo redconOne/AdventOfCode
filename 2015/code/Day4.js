5f0658a73614096cc63d6978d0f486869219d99e
redconOne <mingleeng@gmail.com>
2023-03-25 15:47:54 -0600

    add: solution for Day4 of 2015

---
+++ 2015/code/Day4.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const data = fs.readFileSync('./2015/input/Day4.txt', { encoding: 'utf8' });
+const crypto = require('crypto');
+
+const isValid = (key, num) => {
+  const hash = crypto.Hash('md5');
+  hash.update(key);
+  return hash.digest('hex').startsWith('0'.repeat(num));
+};
+
+const solve1 = (input) => {
+  let suffix = 0;
+
+  while (!isValid(input + suffix, 5)) suffix++;
+
+  return suffix;
+};
+
+const solve2 = (input) => {
+  let suffix = 0;
+
+  while (!isValid(input + suffix, 6)) suffix++;
+
+  return suffix;
+};
+
+console.log(solve1(data));
+console.log(solve2(data));
