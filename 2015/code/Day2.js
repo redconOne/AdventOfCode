3406b486111c11ae73167c0591ba259a96db5c39
redconOne <mingleeng@gmail.com>
2023-05-11 09:29:12 -0600

    fix: solution 2 was somehow missing? also fixed import

--- 2015/code/Day2.js
+++ 2015/code/Day2.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
-const data = fs
-  .readFileSync('./2015/input/Day2.txt', { encoding: 'utf8' })
+import { readFileSync } from 'fs';
+const data = readFileSync('./2015/input/Day2.txt', { encoding: 'utf8' })
   .replace(/\r/g, '')
   .trim();
 
@@ -23,7 +22,23 @@ const solve1 = (input) => {
   return requiredPaper;
 };
 
-const test1 = '1x1x10';
+const calcPaperV2 = (input) => {
+  const regex = /(?<l>\d+)x(?<w>\d+)x(?<h>\d+)/;
+  const { l, w, h } = input.match(regex).groups;
+  const max = Math.max(+l, +w, +h);
+  const ribbon = (+l + +w + +h - max) * 2;
+  const bow = +l * +w * +h;
 
-console.log(solve1(test1));
+  return ribbon + bow;
+};
+
+const solve2 = (input) => {
+  let requiredPaper = 0;
+
+  for (const gift of input.split('\n')) requiredPaper += calcPaperV2(gift);
+
+  return requiredPaper;
+};
+
 console.log(solve1(data));
+console.log(solve2(data));
