182ab173fbb8cd38e65bcb02117b4cc7dcd29fef
redconOne <mingleeng@gmail.com>
2023-05-13 09:33:07 -0600

    refactor: import readFileSync instead of declaring fs

--- 2015/code/Day5.js
+++ 2015/code/Day5.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
-const data = fs
-  .readFileSync('./2015/input/Day5.txt', { encoding: 'utf-8' })
+import { readFileSync } from 'fs';
+const data = readFileSync('./2015/input/Day5.txt', { encoding: 'utf-8' })
   .replace(/\r/g, '')
   .trim();
 
